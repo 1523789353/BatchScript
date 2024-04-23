96c6cf6c718b055aac5dd60155b64c1e88037763
1523789353 <1523789353@qq.com>
2024-04-23 12:40:25 +0800

    更新文本/注释

--- lufs.bat
+++ lufs.bat
@@ -1,29 +1,26 @@
+:: coding: gb2312
+:: @see https://www.bilibili.com/read/cv17301168/
 @echo off
 call :main %*
 exit /b %ErrorLevel%
 
-:main
-    :: 新闻/谈话: -18 LUFS
-    :: 流行音乐: -16 LUFS
-    :: 混合格式: -17 LUFS
-    :: 体育运动: -17 LUFS
-    :: 戏剧: -17 LUFS
-
+:main <音频文件路径> [目标响度]
+    :: 综合响度、真峰值、响度范围
+    set loudness=%~2
+    set truepeak=-0.5
+    set loudrange=11
     :: 在有损编码（lossy encoding）之前，真峰值不应超过-1dBTP。
     :: 这是因为有损编码容易带来峰值过冲（peak overshoot），而且一般码率越低过冲的可能幅度越大。
 
-    :: 综合响度、真峰值、响度范围
-    set loudness=-16
-    set truepeak=-0.5
-    set loudrange=11
+    if "%loudness%" == "" set loudness=-16
 
-    if "%~1" == "" (
-        echo=用法: %~n0 ^<音频文件^>
-        exit /b 1
-    )
+    if "%1" == "" goto :usage
+    if "%1" == "-h" goto :usage
+    if "%1" == "--help" goto :usage
+    if "%1" == "/?" goto :usage
 
     if not exist "%~1" (
-        echo=文件 "%~1" 不存在
+        echo=找不到文件 "%~1"
         exit /b 1
     )
 
@@ -55,9 +52,12 @@ exit /b %ErrorLevel%
     ) else (
         set bit_pcm=%bit_depth%
     )
-    echo 使用 %bit_pcm% 位进行标准化处理
+    echo=使用 %bit_pcm% 位进行标准化处理
+    echo=
+    echo=1-pass: 分析音频流并获取标准化参数
 
     :: 第一遍：分析音频流并获取标准化参数
+    :: 无法将管道符传递给call, 所以使用变量传递命令行
     set "eval.cmdline=ffmpeg -hide_banner -i "%~1" -c:a:0 pcm_s%bit_pcm%le -af "loudnorm=I=%loudness%:TP=%truepeak%:LRA=%loudrange%:print_format=json" -f null - 2>&1 | findstr "\{ \"\ \:\ \" }" | find /v "#""
     call :eval %%%%eval.cmdline%%%%
 
@@ -72,7 +72,6 @@ exit /b %ErrorLevel%
     call :readVal "eval.result[9]" NT
     call :readVal "eval.result[10]" TO
 
-    echo=
     echo 输入综合响度: %II% LUFS
     echo 输入真峰值:   %ITP% dBTP
     echo 输入响度范围: %ILRA% LU
@@ -85,12 +84,24 @@ exit /b %ErrorLevel%
     echo 目标偏移:     %TO% LU
     echo=
 
+
     :: 第二遍：应用参数对音频流进行标准化
     :: 如果处于动态模式，采样率将为 192kHz
-    :: 将流传输给 QAAC 进行编码
-    :: 自动通过内置 SoX 进行向下采样至 48kHz
+    echo=2-pass: 音频标准化
     ffmpeg -hide_banner -y -i "%~1" -c:a pcm_s%bit_pcm%le -af "loudnorm=I=%loudness%:TP=%truepeak%:LRA=%loudrange%:measured_I=%II%:measured_tp=%ITP%:measured_LRA=%ILRA%:measured_thresh=%IT%:offset=%TO%:print_format=summary" -f wav "%~dpn1_%loudness%LUFS.wav" -v quiet
+    echo=完成.
+    explorer /select,"%~dpn1_%loudness%lufs.wav"
+exit /b
+
+:usage
+    echo=用法: %~n0 ^<音频文件路径^> [目标响度:默认-16]
     echo=
+    echo=响度建议:
+    echo=新闻/谈话:  -18 LUFS
+    echo=流行音乐:   -16 LUFS
+    echo=混合格式:   -17 LUFS
+    echo=体育运动:   -17 LUFS
+    echo=戏剧:       -17 LUFS
 exit /b
 
 :readVal <in> <out>
@@ -101,7 +112,7 @@ exit /b
     set "%~2=%readVal.pair:,=%"
 exit /b
 
-:eval
+:eval <...cmdline>
     if defined eval.line (
         for /l %%i in (0,1,%eval.line%) do (
             set "eval.result[%%i]="
